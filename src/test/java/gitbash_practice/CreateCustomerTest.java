8b56dab1a0881aa457333b75b0caf48b2de6f587
suneelmk <suneelkittali@gmail.com>
2025-03-11 15:52:10 +0530

    update

---
+++ src/test/java/gitbash_practice/CreateCustomerTest.java
@@ -0,0 +1,13 @@
+package gitbash_practice;
+
+import org.testng.annotations.Test;
+
+public class CreateCustomerTest {
+
+	
+	@Test
+	public void createCustomerTest()
+	{
+		System.out.println("customer created");
+	}
+}
